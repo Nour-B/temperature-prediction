acd78abb77ef8ec33369674181d581a452954836
Nour Bouzouita <nourelhouda.bouzouita@gmail.com>
2024-12-01 22:18:52 +0100

    fix: mlflow backend connection

--- docker/scripts/start-mlflow-tracking-server.sh
+++ docker/scripts/start-mlflow-tracking-server.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 mlflow server \
     --host 0.0.0.0 \
     --port 8080 \
-    --backend-store-uri "${MLFLOW_BACKEND_STORE}" \
+    --backend-store-uri "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${DB_HOST}/${POSTGRES_DB}" \
     --artifacts-destination "${MLFLOW_ARTIFACT_STORE}"
    
