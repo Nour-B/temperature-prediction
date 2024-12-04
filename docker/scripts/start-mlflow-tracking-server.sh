bd7b58a92bc7b78424864469d346562de7f2b595
Nour Bouzouita <nourelhouda.bouzouita@gmail.com>
2024-12-04 22:41:00 +0100

    chore: add mlflow auth

--- docker/scripts/start-mlflow-tracking-server.sh
+++ docker/scripts/start-mlflow-tracking-server.sh
@@ -6,6 +6,6 @@ mlflow server \
     --app-name basic-auth \
     --host 0.0.0.0 \
     --port 8080 \
-    --backend-store-uri "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${DB_HOST}/${POSTGRES_DB}" \
+    --backend-store-uri "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${DB_HOST}:5432/${POSTGRES_DB}" \
     --artifacts-destination "${MLFLOW_ARTIFACT_STORE}"
    
